ffefaa7177e2983b312981208eb2e4236dc9200b
David Mitchell <davmitchell@deloitte.com.au>
2020-03-18 17:29:39 +1100

    Added append-stub.ps1

---
+++ append-stub.ps1
@@ -0,0 +1,62 @@
+if ($args.Count -ne 3) {
+    Write-Output "Expecting to be invoked with 'pwsh append-stub.ps1 HTTP_VERB URL HTTP_RESPONSE_CODE''"
+    exit 1
+}
+$json = @($Input) | ConvertFrom-Json
+#$new_body = [IO.File]::ReadAllText($args[0])
+$method = $args[0]
+$url = [uri]$args[1]
+$scheme = $url.Scheme
+$destination = $url.Authority
+$path = $url.LocalPath
+$body = ""
+$status = $args[2]
+
+#$json.data.pairs[-1].request.body = $new_body
+$new_stub = [ordered]@{
+    request = [ordered]@{
+        path = @(
+            @{
+                matcher = "exact";
+                value = $path;
+            };
+        )
+        method = @(
+            @{
+                matcher = "exact";
+                value = $method;
+            };
+        )
+        destination = @(
+            @{
+                matcher = "exact";
+                value = $destination;
+            };
+        )
+        scheme = @(
+            @{
+                matcher = "exact";
+                value = $scheme;
+            };
+        )
+        body = @(
+            @{
+                matcher = "exact";
+                value = $body;
+            };
+        )
+        headers = @{};
+        query = @{}
+    };
+    response = [ordered]@{
+        status = [int]$status;
+        body = "nothing";
+        encodedBody = $false;
+        headers = @{};
+        templated = $false;
+
+    }
+};
+
+$json.data.pairs += $new_stub
+Write-Output $json | ConvertTo-Json -Depth 10
