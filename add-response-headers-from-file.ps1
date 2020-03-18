3f5660ffd4e33becc70ab5b466b2647d86f59921
David Mitchell <davmitchell@deloitte.com.au>
2020-03-18 18:12:12 +1100

    Bug fix

--- add-response-headers-from-file.ps1
+++ add-response-headers-from-file.ps1
@@ -5,5 +5,5 @@ if ($args.Count -ne 1) {
 }
 $json = @($Input) | ConvertFrom-Json
 $new_headers = [IO.File]::ReadAllText($args[0]) | ConvertFrom-Json
-$json.data.pairs[0].response.headers = $new_headers
+$json.data.pairs[-1].response.headers = $new_headers
 Write-Output $json | ConvertTo-Json -Depth 10
