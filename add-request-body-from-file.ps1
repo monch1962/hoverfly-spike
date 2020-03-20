2251a8de300f8a8fe1f559d666518d768f68827e
David Mitchell <davmitchell@deloitte.com.au>
2020-03-21 09:31:40 +1100

    Bug fix

--- add-request-body-from-file.ps1
+++ add-request-body-from-file.ps1
@@ -1,10 +1,27 @@
+Import-Module -Name Microsoft.PowerShell.Utility
+
 if ($args.Count -ne 1) {
-    Write-Output "Expecting to be invoked with 'pwsh add-request-body-from-file.ps1 JSON_REQUEST_BODY'"
+    Write-Output "Expecting to be invoked with 'pwsh add-request-body-from-file.ps1 REQUEST_BODY'"
     Write-Output "e.g. 'pwsh add-request-body-from-file.ps1 request-body.json'"
     exit 1
 }
 $json = @($Input) | ConvertFrom-Json
 $new_body = [IO.File]::ReadAllText($args[0])
+#if ($new_body | Test-Json) {
+#    $new_body = ConvertTo-Json $new_body
+#}
+try {
+    $ignore = ConvertFrom-Json $new_body -ErrorAction Stop;
+    $bodyIsJson = $true;
+} catch {
+    $bodyIsJson = $false;
+}
+if ($bodyIsJson) {
+    $new_body = ConvertFrom-Json $new_body
+    $json.data.pairs[-1].request.body[-1].matcher = "json"
+} else {
+    $json.data.pairs[-1].request.body[-1].matcher = "exact"
+}
 $json.data.pairs[-1].request.body[-1].value = $new_body
-$json.data.pairs[-1].request.body[-1].matcher = "exact"
+
 Write-Output $json | ConvertTo-Json -Depth 10
